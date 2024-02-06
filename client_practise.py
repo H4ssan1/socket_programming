c8950797d69e6d63bfcea8766b2fbfcf309d7465
Hassan Shugaa <h.shugaa@se21.qmul.ac.uk>
2024-02-06 17:52:36 +0000

    added client practise

---
+++ client_practise.py
@@ -0,0 +1,21 @@
+import socket
+import os
+client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#host=socket.gethostname() #server hostname
+server_IP='192.168.0.182'
+port=12000 #same as server
+client.connect((server_IP,port))
+
+while True:
+
+    inp = input("enter message\n")
+    client.send(inp.encode())
+    print("waiting for response...\n")
+    if not inp:
+        break
+
+    answer = client.recv(1024)
+    print("response is: "+answer.decode())
+
+client.close()
+    
