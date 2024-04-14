8ee96d52fb5ae089fdeda19b45f408d5767c2901
H4ssan1 <96207866+H4ssan1@users.noreply.github.com>
2024-04-14 15:31:29 +0100

    server_instance updated quite well
    plus client main now can send messages to servers

--- client_main.py
+++ client_main.py
@@ -20,10 +20,9 @@ server_connections = {} ##dictionary to store client connections
 def server_handler(conn, addr):
     print(f"NEW CONNECTION {addr} CONNECTED")
     connected = True
-    calculation = input("Input to send to client:\n")
-    conn.send(calculation.encode(format))
 
     while connected:
+        
         msg_length = conn.recv(HEADER).decode(format)
         if msg_length:
             msg_length = int(msg_length)
@@ -34,6 +33,21 @@ def server_handler(conn, addr):
     
     conn.close()
 
+def send_to_client(client_addr, message):
+    if client_addr in server_connections:
+        client_conn = server_connections[client_addr]
+        client_conn.send(message.encode(format))
+    else:
+        print("Client not found.")
+
+def if_send_to_client():
+        server_input = input("Would you like to send a message to a client? (y/n)")
+        if server_input == "y":
+            client_IP = input("enter IP of client\n")
+            client_port = int(input("enter port of client\n"))
+            client_address = (client_IP, client_port)
+            message_to_send = input("Enter calculation to send\n")
+            send_to_client(client_address, message_to_send)
 
 def start():
     server.listen()
@@ -51,6 +65,11 @@ def start():
         elif (threading.active_count() - 1 == 0):
             print("No connections. Closing server...")
             break
+
+        print(f"Client addresses: {server_connections}")
+        if_send_to_client()
+                
+
 
 print("[STARTING] server is starting...")
 start()
--- server_instance.py
+++ server_instance.py
@@ -1,5 +1,6 @@
 import socket
-
+import select
+import time
 
 port_no = 12000
 HEADER = 64
@@ -7,3 +8,68 @@ host_IP = "192.168.0.72"
 format = 'UTF-8'
 DISCONNECT_MESSAGE = "!BREAK"
 ADDR = (host_IP, port_no)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(ADDR)
+
+def server_running():
+    while True:
+        # Check if there's data to be received
+        ready_to_read, _, _ = select.select([server], [], [], 30)
+        if not ready_to_read: 
+            print("Nothing has been received for 30 seconds. Exiting...")
+            break
+        for sock in ready_to_read:
+            if sock == server:
+                message_received = server.recv(2048).decode(format)
+                if message_received:
+                    print(f"Algorithm sent from client ({ADDR}): {message_received}")
+                    process_algorithms(message_received)
+
+def process_algorithms(algorithm):
+    first_num = ""
+    operation = ""
+    second_num = ""
+    calculated_operation = 0
+    for numerical in algorithm:
+        if operation == "" and (numerical == "+" or numerical == "-" or numerical == "*" or numerical == "/"):
+            operation = numerical
+        elif operation == "":
+            first_num += numerical
+        else:
+            second_num += numerical
+    
+    if operation == "+":
+        calculated_operation = int(first_num) + int(second_num)
+        time.sleep(10)
+        send_to_client(str(calculated_operation))
+
+    elif operation == "-":
+        calculated_operation = int(first_num) - int(second_num)
+        time.sleep(10)
+        send_to_client(str(calculated_operation))
+
+    elif operation == "*":
+        calculated_operation = int(first_num) * int(second_num)
+        time.sleep(10)
+        send_to_client(str(calculated_operation))
+
+    elif operation == "/":
+        calculated_operation = int(first_num) / int(second_num)
+        time.sleep(10)
+        send_to_client(str(calculated_operation))
+
+    else:
+        print("No operation was found")
+        send_to_client("Resend calculation. No operation was found")
+
+
+def send_to_client(result):
+    message = result.encode(format)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(format)
+    send_length += b' ' * (HEADER - len(send_length))
+    server.send(send_length)
+    server.send(message)
+
+server_running()
